f8f47e6dcf3572a12310c95452696b76c53a9ea2
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-10-26 13:06:09 +0900

    feat: supabaseのクライアントを生成するカスタムフックスの作成

---
+++ pointRanking/src/hooks/useSupabase.ts
@@ -0,0 +1,33 @@
+import { load } from "ts-dotenv";
+import { Database } from "../../database.types";
+import { createClient } from "@supabase/supabase-js";
+
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+
+const env = load({
+  SUPABASE_URL: {
+    type: String,
+    default: process.env.SUPABASE_URL || '',
+  },
+  SUPABASE_ANON_KEY: {
+    type: String,
+    default: process.env.SUPABASE_ANON_KEY || '',
+  },
+})
+
+export const useSupabase = () => {
+  const SUPABASE_URL = env.SUPABASE_URL || '';
+  const SUPABASE_ANON_KEY = env.SUPABASE_ANON_KEY || '';
+  
+  if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+    throw new Error('SUPABASE_URLかSUPABASE_ANON_KEYの環境変数に問題あり!!')
+  }
+
+  const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    }
+  })
+}
