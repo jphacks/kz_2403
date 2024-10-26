7d1ecbadba28036dc931081d5b92db96ca53f845
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-10-26 11:29:54 +0900

    feat: add-emoji

--- emoji-recommend-workers/src/index.ts
+++ emoji-recommend-workers/src/index.ts
@@ -1,9 +1,42 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Env = {
+  Bindings: {
+    VECTORIZE: VectorizeIndex
+    // Hugging Face + AI Gateway
+    HF_API_TOKEN: string
+    CF_ACCOUNT_ID: string
+    CF_AI_GATEWAY_ID: string
+  }
+}
+
+const app = new Hono<Env>()
 
 app.get('/ping', (c) => {
   return c.text('pong')
 })
+
+// 新しい絵文字とそのEmbeddingを追加するエンドポイント
+app.put('/add-emoji', async (c) => {
+  // example: { emoji: "ultrafast-parrot", context: "テンションが高い 楽しい 嬉しい" }
+  const { id, emoji, context } = await c.req.json<{ id: string, emoji: string; context: string }>();
+
+  const body = JSON.stringify({ inputs: context });
+  
+  const response = await fetch(`https://gateway.ai.cloudflare.com/v1/${c.env.CF_ACCOUNT_ID}/${c.env.CF_AI_GATEWAY_ID}/huggingface/intfloat/multilingual-e5-large`, {
+    headers: {
+      Authorization: `Bearer ${c.env.HF_API_TOKEN}`,
+      ContentType: "application/json",
+    },
+    method: "POST",
+    body: body,
+  })
+
+  const embedded: number[] = await response.json();
+
+  await c.env.VECTORIZE.insert([{ id: id, values: embedded, namespace: 'emoji', metadata: { name: emoji } }]);
+
+  return c.json({ data: { id, embedded: embedded } }, 201);
+});
 
 export default app
