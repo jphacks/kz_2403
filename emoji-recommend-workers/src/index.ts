b4a9fb7c6f1f58b0371e5a327fb4939480d8b934
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-10-26 11:39:30 +0900

    feat: recommend

--- emoji-recommend-workers/src/index.ts
+++ emoji-recommend-workers/src/index.ts
@@ -39,4 +39,47 @@ app.put('/add-emoji', async (c) => {
   return c.json({ data: { id, embedded: embedded } }, 201);
 });
 
+app.post('/recommend', async (c) => {
+  try {
+    // { id: 投稿ID, text: 投稿内容 }
+    const { id, text } = await c.req.json<{ id: string, text: string }>();
+    if (!text) {
+      return c.json({ error: 'Bad Request: text is not defined' }, 400);
+    }
+
+    const body = JSON.stringify({ input: text });
+    const response = await fetch(`https://gateway.ai.cloudflare.com/v1/${c.env.CF_ACCOUNT_ID}/${c.env.CF_AI_GATEWAY_ID}/huggingface/intfloat/multilingual-e5-large`, {
+      headers: {
+				Authorization: `Bearer ${c.env.HF_API_TOKEN}`,
+				ContentType: "application/json",
+			},
+			method: "POST",
+			body: body,
+    })
+    
+    const embedded: number[] = await response.json();
+    
+    // emojiとの類似度を計算
+    const result = await c.env.VECTORIZE.query(embedded, {
+      topK: 3,
+      namespace: 'emoji',
+      returnMetadata: "all",
+    })
+    
+    await c.env.VECTORIZE.insert([{ id: id, values: embedded, namespace: 'message' }]);
+
+    const recommendReactions = result.matches.map((match) => (
+      {
+        id: match.id,
+        emoji: match?.metadata?.name ?? '',
+        score: match.score,
+      }
+    ))
+    return c.json({ recommendReactions }, 200);
+  } catch (e) {
+    console.error(e);
+    return c.json({ error: 'Error' }, 500);
+  }
+})
+
 export default app
