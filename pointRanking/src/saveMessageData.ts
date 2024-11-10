c41e60a854ad36d527d4c50091f23dd64e992957
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-10 18:09:22 +0900

    fix: MessageNewテーブルに対応

--- pointRanking/src/saveMessageData.ts
+++ pointRanking/src/saveMessageData.ts
@@ -2,8 +2,9 @@ import { useSupabase } from "./hooks/useSupabase";
 
 interface Payload {
   messageId: string;
+  workspaceId: string;
   messageText: string;
-  messageUserId: string;
+  userId: string;
   channelId: string;
 }
 
@@ -12,22 +13,24 @@ export const saveMessageData = async (payload: Payload): Promise<boolean> => {
 
   // メッセージが存在するか確認
   const { data: messageExists, error: selectError } = await supabase
-    .from("Message")
+    .from("MessageNew")
     .select("message_id")
     .eq("message_id", payload.messageId)
+    .eq("workspace_id", payload.workspaceId)
     .single();
 
   // PGRST116エラーの場合はメッセージが存在しないため、新しいメッセージを挿入
   if (selectError && selectError.code === "PGRST116") {
     const { error: insertMessageError } = await supabase
-      .from("Message")
+      .from("MessageNew")
       .insert([
         {
           message_id: payload.messageId,
-          created_at: new Date().toISOString(),
+          workspace_id: payload.workspaceId,
           message_text: payload.messageText,
-          message_user_id: payload.messageUserId,
-          channnel_id: payload.channelId,
+          user_id: payload.userId,
+          channel_id: payload.channelId,
+          created_at: new Date().toISOString(),
           update_at: new Date().toISOString(),
         },
       ]);
