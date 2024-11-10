45d241c2356fe5821f69e2ffde386ffe57458cb1
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-11-10 17:43:24 +0900

    fix: 新しい絵文字テーブルに追加できる様に変更

--- pointRanking/src/ensureEmojiExits.ts
+++ pointRanking/src/ensureEmojiExits.ts
@@ -8,7 +8,7 @@ export const ensureEmojiExists = async (
   const { supabase } = useSupabase();
 
   const { error } = await supabase
-    .from("Emoji")
+    .from("EmojiNew")
     .select("emoji_id")
     .eq("workspace_id", workspaceId)
     .eq("emoji_id", emojiId)
@@ -17,13 +17,13 @@ export const ensureEmojiExists = async (
   // errorオブジェクトが存在して、エラーコードがPGRST116の場合
   if (error && error.code === "PGRST116") {
     const { error: insertError } = await supabase
-      .from("Emoji")
-      .insert([{ workspace_id: workspaceId, emoji_id: emojiId, emoji_name: emojiName }]);
+      .from("EmojiNew")
+      .insert([{ workspace_id: workspaceId, emoji_id: emojiId, emoji_name: emojiName, label: ''}]);
 
     if (insertError) {
-      console.error("Emojiテーブルの挿入エラー:", insertError);
+      console.error("EmojiNewテーブルの挿入エラー:", insertError);
     }
   } else if (error) {
-    console.error("Emojiテーブルの取得エラー:", error);
+    console.error("EmojiNewテーブルの取得エラー:", error);
   }
 };
